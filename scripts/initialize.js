468f4f95bcf86a14dc0c1378ef36f9e3eb171ff6
Katherine Hegewisch <khegewisch@ucmerced.edu>
2023-09-12 23:22:58 -0700

    added PDF export - cross domain issue

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -38,6 +38,40 @@ function downloadReport(){
 	};
 };
 
+
+ function export_to_pdf(div_id) {
+        var dom_el = document.getElementById(div_id);
+        var downloadFileName = 'downloadFile.pdf';
+
+        html2canvas(dom_el, {
+            useCORS: true,
+           }).then(function (canvas) {
+                var h=$('#'+div_id).height();
+                var w=$('#'+div_id).width();
+
+                var img = canvas.toDataURL("image/png,1.0");
+		 //img.setAttribute('crossorigin', 'anonymous');
+                if(w>h){
+                        var orient = 'landscape';
+                }else{
+                        var orient = 'portrait';
+                };
+
+                var pad_h = 50;
+                var pad_w = 50;
+
+                var pdf = new jsPDF(orient, 'pt', [h + pad_h, w + pad_w]);
+
+                    //addImage(imageData, format, x,y, width, height, alias, compression, rotation
+                    //x,y are 5pt padding on the left or upper edge of page
+                    pdf.addImage(img, 'PNG', 5, 5, w, h);
+
+                    //add credits at bottom of page
+                    pdf.setFontSize(12);
+                    pdf.save(downloadFileName);
+        })
+};
+
 <!------------------------------------>
 <!--    FUNCTION: INITIALIZE     -->
 <!------------------------------------>
